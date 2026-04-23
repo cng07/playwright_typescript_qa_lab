a1d19cdd8e771308557849c18fa6117581590a42
carlosNg <carlosng07@gmail.com>
2026-04-23 23:40:42 +0800

    chore: dotenv on config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
 
 /**
  * Read environment variables from file.
@@ -34,12 +36,14 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: process.env.CI ? 'retain-on-failure' : 'off',
     headless: process.env.CI ? true : false,
+    
   },
 
   /* Configure projects for major browsers */
