77a8c850c4ebb7210cc9e23f77b0eb77da1ab8f3
carlosNg <carlosng07@gmail.com>
2026-04-24 21:52:59 +0800

    test: add tests for db

--- tests/db/users.db.spec.ts
+++ tests/db/users.db.spec.ts
@@ -2,6 +2,25 @@ import { test, expect } from '@playwright/test';
 import { queryDB } from '../../utils/dbClient';
 
 test('Verify users exist in DB', async () => {
-  const users = await queryDB('SELECT * FROM users');
-  expect(users.length).toBeGreaterThan(0);
+    const users = await queryDB('SELECT * FROM users');
+    expect(users.length).toBeGreaterThan(0);
+});
+
+
+test('Verify users have required fields', async () => {
+    const users = await queryDB('SELECT * FROM users LIMIT 1');
+
+    expect(users[0]).toHaveProperty('id');
+    expect(users[0]).toHaveProperty('first_name');
+    expect(users[0]).toHaveProperty('last_name');
+    expect(users[0]).toHaveProperty('email');
+    expect(users[0]).toHaveProperty('created_at');
+    expect(users[0]).toHaveProperty('Nationality');
+    expect(users[0]).toHaveProperty('Role');
+});
+
+test('Verify emails should not be null', async () => {
+    const users = await queryDB('SELECT * FROM users WHERE email IS NULL');
+
+    expect(users.length).toBe(0);
 });
